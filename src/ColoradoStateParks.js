0c99ebb99b16a943dc4104034d36b4a87f1def62
ayubdev <ayubgardy898@gmail.com>
2022-09-13 22:09:14 +0300

    Done the assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,12 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import { elevation as Elevation } from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  Elevation();
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+export default ColoradoStateParks;
